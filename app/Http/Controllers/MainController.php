55bff1301372181a1cb1a8d04d8987852342b198
Тима <timur.ggt@gmail.com>
2020-12-10 23:36:45 +0600

    3-4 lessons

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -10,12 +11,20 @@ class MainController extends Controller
         return view('index');
     }
     public function categories() {
-        return view('categories');
+        $categories = Category::get();
+        return view('categories', compact('categories'));
     }
-    public function category($category) {
+    public function category($code) {
+        $category = Category::where('code', $code)->first();
         return view('category', compact('category'));
     }
-    public function product($product = null) {
+    public function product($category, $product = null) {
         return view('product', ['product' => $product]);
+    }
+    public function basket() {
+        return view('basket');
+    }
+    public function order() {
+        return view('order');
     }
 }
--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,9 +1,9 @@
 @extends('master')
 
-@section('title', 'Главная')
+@section('title', 'Товар')
 
 @section('content')
     <div class="container">
-        <h1 class="my-4">index</h1>
+        <h1 class="my-4">BASKET</h1>
     </div>
 @endsection
